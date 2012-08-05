e764da77d9f115fe7c9a70fe456d28c262923ce5
kithaka <kithakaanto@gmail.com>
2012-08-05 11:53:45 +0300

    Finish layouts and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,32 +3,42 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have the h1 'Sample App' " do
-       visit '/static_pages/home'
+       visit root_path
        page.should have_selector('h1', :text =>'Sample App')
        end
     it "should have the title 'Home' " do
-       visit '/static_pages/home'
+       visit root_path
         page.should have_selector('title', :text=>"Home")
        end
     end
   describe "Help page" do
     it "should have the h1'Help' " do
-       visit '/static_pages/help'
+       visit help_path
        page.should have_selector('h1', :text=>"Help")
       end
      it "should have the title 'Help' "do
-       visit '/static_pages/help'
+       visit help_path
         page.should have_selector('title', :text=>"Help")
      end
   end
  describe "About page" do
   it "should have the h1'About us' " do
-   visit '/static_pages/about'
+   visit about_path
    page.should have_selector('h1', :text=>"About us")
   end
   it "should have the title 'About us' "do
-    visit '/static_pages/about'
+    visit about_path
     page.should have_selector('title', :text=>"About us")
   end
  end
+  describe "Contact page"do
+   it "should have the h1'Contact'"do
+     visit contact_path
+      page.should have_selector('h1', :text=>"Contact")
+    end
+   it "should have the title 'Contact'"do
+     visit contact_path
+      page.should have_selector('title', :text=>"Contact")
+    end
+  end
 end
---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "UserPages" do
+  describe "Signup page" do
+    it "should have the title'Sign up'" do
+     visit signup_path
+     page.should have_selector('title', :text=>'Sign up')
+    end
+   it "should have the h1 'Sign up'"do
+    visit signup_path
+    page.should have_selector('h1', :text=>'Sign up')
+   end
+  end
+end
